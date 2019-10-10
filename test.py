985eeec55ed631733a585a5527d3f25b6f21da24
skiteskopes <billzhang8@gmail.com>
2019-10-09 17:28:38 -0700

    modified

--- test.py
+++ test.py
@@ -3,21 +3,19 @@ import numpy as np
 import cv2
 import os
 import argparse
-global IR_hash, height, width
+global IR_dict, height, width, directory
+from skimage.measure import compare_ssim as ssim
 directory = r'E:/20190919_022250'
 os.chdir(directory)
-irfile = r'E:/20190919_022250/20190919_022255_858_IR.TIFF'
-array = tifffile.imread(irfile)
-print(array)
-height, width = array.shape
-print(height,width)
-IR_hash = []
+IR_dict = {}
 def image_opener():
     '''opens an image and then stores the array in a dictionary'''
-    for image in directory:
-        array = tiffile.imread(image)
-        height, width = array.shape
-        IR_hash[]
+    for image in os.listdir(directory):
+        if image.endswith(".TIFF"):
+            array = tifffile.imread(image)
+            IR_dict[image]=array
+    print(IR_dict)
+image_opener()
 def mean_squared_error(a,b):
     # the 'Mean Squared Error' between the two images is the
 	# sum of the squared difference between the two images;
@@ -26,9 +24,14 @@ def mean_squared_error(a,b):
     h2,w2 = b.shape
     if h1 != h2 or w1 != w2:
         print('ERROR: Mean squared test failed because image.shape are of different dimensions')
+        return 1
     else:
-         err = np.sum((a.astype("float") - b.astype("float")) ** 2)
-	     err /= float(a.shape[0] * a.shape[1])
+        err = np.sum((a.astype("float") - b.astype("float")) ** 2)
+        err /= float(a.shape[0] * a.shape[1])
+    return err
+image_opener()
+def compare_images():
+    for image in IR_dict: 
+        print(image)
 
-
-    
+compare_images()
